dbe22223ecb8dfbe171d752781595e13bd448a57
Arleee1 <eeermo@gmail.com>
2021-02-06 10:38:55 -0500

    Added support for managing options files

--- minecraft_mod_controller.py
+++ minecraft_mod_controller.py
@@ -1,47 +1,101 @@
 import os
 import shutil
+import typing
 
 
 class ModController:
-    def __init__(self):
+    def __init__(self) -> None:
+
+        # Set constants
         self.MINECRAFT_DIR = r"C:\Users\eeerm\AppData\Roaming\.minecraft"
+
         self.MODS_FOLDERS_DIR = os.path.join(self.MINECRAFT_DIR, r"mods_folders")
         self.MODS_DIR = os.path.join(self.MINECRAFT_DIR, r"mods")
+
+        self.OPTIONS_FOLDER_DIR = os.path.join(self.MINECRAFT_DIR, r"options_folder")
+        self.OPTIONS_FILE = os.path.join(self.MINECRAFT_DIR, r"options.txt")
+
         self.refresh()
 
-    def refresh(self):
+    def refresh(self) -> None:
+        """
+        Ensure the proper folders exist
+        :return: None
+        """
+
         if not os.path.isdir(self.MINECRAFT_DIR):
             raise NotADirectoryError(".minecraft is not a directory")
 
-        if not os.path.isdir(self.MODS_FOLDERS_DIR):
-            if not os.path.isfile(self.MODS_FOLDERS_DIR):
-                os.mkdir(self.MODS_FOLDERS_DIR)
-            else:
-                raise NotADirectoryError("mods_folders is not a directory")
+        self.verify_folder(self.MODS_FOLDERS_DIR, "mods_folders")
+        self.verify_folder(self.MODS_DIR, "mods")
+        self.verify_folder(self.OPTIONS_FOLDER_DIR, "options_folder")
 
-        if not os.path.isdir(self.MODS_DIR):
-            if not os.path.isfile(self.MODS_DIR):
-                os.mkdir(self.MODS_DIR)
-            else:
-                raise NotADirectoryError("mods_folders is not a directory")
+    def verify_folder(self, folder: str, folder_name: str) -> None:
+        """
+        Check if each folder exists, and create it if it doesn't
+        :param folder: Folder to be checked
+        :param folder_name: Display name of the folder
+        :return: None
+        """
 
-    def transfer_mods_folder(self, folder):
+        if not os.path.isdir(folder):
+            if not os.path.isfile(folder):
+                os.mkdir(folder)
+            else:
+                raise NotADirectoryError(f"{folder_name} is not a directory")
+
+    def transfer_mods_folder(self, folder: str) -> None:
+        """
+        Makes a link to the mods folder to be used by Forge
+        :param folder: Mods folder to be linked to
+        :return: None
+        """
+
         src_dir = os.path.join(self.MODS_FOLDERS_DIR, folder)
 
         if not os.path.isdir(src_dir):
             raise NotADirectoryError(f"{src_dir} is not a directory")
 
         # TODO Perhaps implement a check to verify if mods dir is backed up in mods_folder dir, so that mods dir doesnt get deleted without saving
-        shutil.rmtree(self.MODS_DIR)
+        os.unlink(self.MODS_DIR)
         dst_dir = self.MODS_DIR
-        shutil.copytree(src_dir, dst_dir)
+        os.symlink(src_dir, dst_dir)
 
-    def get_mods_folders(self):
-        return os.listdir(self.MODS_FOLDERS_DIR)
+    def get_mods_folders(self) -> typing.List[str]:
+        """
+        Retrieves the folders in the mods_folders directory
+        :return: A list of the folders in the mods_folders directory
+        """
+
+        return list(os.listdir(self.MODS_FOLDERS_DIR))
+
+    def transfer_options_file(self, options_file: str) -> None:
+        """
+        Makes a link to the options file to be used by Minecraft
+        :param options_file: Options file to be linked to
+        :return: None
+        """
+
+        src_file = os.path.join(self.OPTIONS_FOLDER_DIR, options_file)
+
+        if not os.path.isfile(src_file):
+            raise FileNotFoundError(f"{src_file} is not a file")
+
+        # TODO Perhaps implement a check to verify if options file is backed up in options_folder dir, so that the options file doesnt get deleted without saving
+        os.unlink(self.OPTIONS_FILE)
+        dst_dir = self.OPTIONS_FILE
+        os.symlink(src_file, dst_dir)
+
+    def get_options_files(self) -> typing.List[str]:
+        """
+        Retrieves the files in the options_folder directory
+        :return: A list of the folders in the options_folder directory
+        """
+
+        return list(os.listdir(self.OPTIONS_FOLDER_DIR))
 
 
 if __name__ == "__main__":
-    # TODO Implement GUI
     mod_controller = ModController()
-    mod_controller.transfer_mods_folder("mods 1.16.3 create")
     print(mod_controller.get_mods_folders())
+    print(mod_controller.get_options_files())
--- minecraft_mod_controller_gui.py
+++ minecraft_mod_controller_gui.py
@@ -1,11 +1,12 @@
 import sys
+import PyQt5
 from PyQt5.QtWidgets import *
 import minecraft_mod_controller as mmc
 
 
 class ModControllerGUI(QMainWindow):
 
-    def __init__(self):
+    def __init__(self) -> None:
         super(ModControllerGUI, self).__init__()
 
         self.title = "Mod Manager"
@@ -14,42 +15,103 @@ class ModControllerGUI(QMainWindow):
         self.width = 640
         self.height = 480
 
-        self.selected_item = None
+        self.selected_mods_folder = None
+        self.selected_options_file = None
 
         self.initUI()
         self.show()
 
-    def initUI(self):
+    def initUI(self) -> None:
+        """
+        Initializes the user interface
+        :return: None
+        """
+
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
 
         mod_controller = mmc.ModController()
+
+        # Create mods area
         mods_folders_list_widget = QListWidget()
         mods_folders_list_widget.addItems(mod_controller.get_mods_folders())
         mods_folders_list_widget.itemClicked.connect(self.mods_folders_list_clicked)
 
-        apply_btn = QPushButton("Apply")
-        apply_btn.pressed.connect(self.apply_btn_pressed)
+        apply_mods_btn = QPushButton("Apply Mods")
+        apply_mods_btn.pressed.connect(self.mods_apply_btn_pressed)
 
+        mods_layout = QVBoxLayout()
+        mods_layout.addWidget(mods_folders_list_widget)
+        mods_layout.addWidget(apply_mods_btn)
+
+        # Create options area
+        options_files_list_widget = QListWidget()
+        options_files_list_widget.addItems(mod_controller.get_options_files())
+        options_files_list_widget.itemClicked.connect(self.options_files_list_clicked)
+
+        apply_options_btn = QPushButton("Apply Options")
+        apply_options_btn.pressed.connect(self.options_apply_btn_pressed)
+
+        options_layout = QVBoxLayout()
+        options_layout.addWidget(options_files_list_widget)
+        options_layout.addWidget(apply_options_btn)
+
+        # Assemble the layouts together
         central_widget = QWidget()
-        central_layout = QVBoxLayout()
-        central_layout.addWidget(mods_folders_list_widget)
-        central_layout.addWidget(apply_btn)
+        central_layout = QHBoxLayout()
+        central_layout.addLayout(mods_layout)
+        central_layout.addLayout(options_layout)
         central_widget.setLayout(central_layout)
         self.setCentralWidget(central_widget)
 
-    def mods_folders_list_clicked(self, item):
-        self.selected_item = item.text()
+    def mods_folders_list_clicked(self, selected_item: PyQt5.QtWidgets.QListWidgetItem) -> None:
+        """
+        Called when mods_folders_list is clicked, and stores the clicked item
+        :param selected_item: Mods list item that was clicked
+        :return: None
+        """
 
-    def apply_btn_pressed(self):
-        if self.selected_item is None:
+        self.selected_mods_folder = selected_item.text()
+
+    def mods_apply_btn_pressed(self) -> None:
+        """
+        Called when the apply button is pressed and attempts to apply the changes to the mods folders
+        :return: None
+        """
+
+        if self.selected_mods_folder is None:
             self.statusBar().showMessage(f"Please select a mods folder first", 5_000)
             return
 
         mod_controller = mmc.ModController()
-        mod_controller.transfer_mods_folder(self.selected_item)
-        print(f"Apply button pressed: {self.selected_item}")
-        self.statusBar().showMessage(f"Loaded mods: {self.selected_item}", 5_000)
+        mod_controller.transfer_mods_folder(self.selected_mods_folder)
+        print(f"Mods apply button pressed: {self.selected_mods_folder}")
+        self.statusBar().showMessage(f"Loaded mods: {self.selected_mods_folder}", 5_000)
+
+    def options_files_list_clicked(self, selected_item: PyQt5.QtWidgets.QListWidgetItem) -> None:
+        """
+        Called when options_files_list is clicked, and stores the clicked item
+        :param selected_item: Options list item that was clicked
+        :return: None
+        """
+
+        self.selected_options_file = selected_item.text()
+
+    def options_apply_btn_pressed(self) -> None:
+        """
+        Called when the options apply button is pressed and attempts to apply the changes to the options file
+        :return: None
+        """
+
+        if self.selected_options_file is None:
+            self.statusBar().showMessage(f"Please select an options file first", 5_000)
+            return
+
+        print(self.selected_options_file)
+        mod_controller = mmc.ModController()
+        mod_controller.transfer_options_file(self.selected_options_file)
+        print(f"Options apply button pressed: {self.selected_options_file}")
+        self.statusBar().showMessage(f"Loaded options: {self.selected_options_file}", 5_000)
 
 
 if __name__ == '__main__':
