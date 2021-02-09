5839eb15b21e737d9e8e3215129982205949241b
Arleee1 <eeermo@gmail.com>
2021-02-09 17:35:07 -0500

    Uses the stored data of the order

--- MMCListWidget.py
+++ MMCListWidget.py
@@ -2,8 +2,8 @@ import PyQt5
 from PyQt5 import QtWidgets, QtCore
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QAbstractItemView
-import json
 import minecraft_mod_controller as mmc
+import typing
 
 
 class MMCListWidget(PyQt5.QtWidgets.QListWidget):
@@ -17,6 +17,8 @@ class MMCListWidget(PyQt5.QtWidgets.QListWidget):
 
         self.label = label
 
+        self.drop_event_func = None
+
     def dropEvent(self, e):
         mod_controller = mmc.ModController()
         print("drop event")
@@ -24,5 +26,18 @@ class MMCListWidget(PyQt5.QtWidgets.QListWidget):
         super(MMCListWidget, self).dropEvent(e)
         self.takeItem(self.currentRow())
         items_text_list = [str(self.item(i).text()) for i in range(self.count())]
-        mod_controller.set_mods_or_options_list(items_text_list, is_mods=(self.label == "mods_widget"))
+        mod_controller.set_mods_or_options_order(items_text_list, is_mods=(self.label == "mods_widget"))
         print(items_text_list)
+        if self.drop_event_func is not None:
+            self.drop_event_func()
+
+    def dragMoveEvent(self, e):
+        super(MMCListWidget, self).dragMoveEvent(e)
+        print("drag move event")
+
+    def dragLeaveEvent(self, e):
+        super(MMCListWidget, self).dragLeaveEvent(e)
+        print("drag leave event")
+
+    def set_drop_event_func(self, func: typing.Callable):
+        self.drop_event_func = func
