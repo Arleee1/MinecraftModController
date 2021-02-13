63662541eb280e9fe75c70c02c8affaa51363514
Arleee1 <61568556+Arleee1@users.noreply.github.com>
2021-02-13 15:17:57 -0500

    Fixed pip to be pip3

--- setup.bat
+++ setup.bat
@@ -1,4 +1,4 @@
 curl -O https://www.python.org/ftp/python/3.9.1/python-3.9.1-amd64.exe
 python-3.9.1-amd64.exe /passive InstallAllUsers=1 PrependPath=1
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 pause
